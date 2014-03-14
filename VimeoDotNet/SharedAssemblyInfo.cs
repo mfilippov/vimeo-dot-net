f4cdd35e46009d7fbcfe1677587bb308fe02ad71
scommisso <steve@stevecommisso.com>
2014-03-13 20:23:31 -0700

    Remove unnecessary project references

--- VimeoDotNet/SharedAssemblyInfo.cs
+++ VimeoDotNet/SharedAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.6.0")]
-[assembly: AssemblyFileVersion("0.6.0")]
+[assembly: AssemblyVersion("0.6.1")]
+[assembly: AssemblyFileVersion("0.6.1")]
