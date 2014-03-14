d1a693785a42b1bbd50c1665731541fa738ffad1
scommisso <steve@stevecommisso.com>
2014-03-13 20:50:57 -0700

    Added NuGet spec

--- VimeoDotNet/SharedAssemblyInfo.cs
+++ VimeoDotNet/SharedAssemblyInfo.cs
@@ -2,6 +2,10 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+[assembly: AssemblyCompany("Stephen Commisso")]
+[assembly: AssemblyProduct("VimeoDotNet")]
+[assembly: AssemblyCopyright("Copyright © 2014, Stephen Commisso")]
+
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
