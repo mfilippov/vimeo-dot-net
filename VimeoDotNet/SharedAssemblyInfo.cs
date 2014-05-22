e46371f56d6fd4cc9ee6271a0fb16eda4508002b
scommisso <scommisso@godaddy.com>
2014-05-22 10:01:05 -0700

    Version Bump

--- VimeoDotNet/SharedAssemblyInfo.cs
+++ VimeoDotNet/SharedAssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.6.5")]
-[assembly: AssemblyFileVersion("0.6.5")]
+[assembly: AssemblyVersion("0.6.6")]
+[assembly: AssemblyFileVersion("0.6.6")]
