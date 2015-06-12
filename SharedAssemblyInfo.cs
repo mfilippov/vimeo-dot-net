c2991be46e5d91bd6a83eb8a07d60e5d6735f320
Mikhail Filippov <mikhail@filippov.me>
2015-06-12 18:44:25 +0300

    Update package version.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -5,10 +5,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("VimeoDotNet")]
 [assembly: AssemblyCopyright("Copyright © 2014, Stephen Commisso")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.7.3")]
-[assembly: AssemblyFileVersion("0.7.3")]
+[assembly: AssemblyVersion("0.7.4")]
+[assembly: AssemblyFileVersion("0.7.4")]
