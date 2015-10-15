de7ca0c793f2fda5c5c70fe128507829cf53e288
scommisso <scommisso@godaddy.com>
2015-10-15 15:56:02 -0700

    Version bump

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyCompany("Stephen Commisso")]
+[assembly: AssemblyCompany("VimeoDotNet")]
 [assembly: AssemblyProduct("VimeoDotNet")]
-[assembly: AssemblyCopyright("Copyright © 2014-2015, Stephen Commisso")]
+[assembly: AssemblyCopyright("Copyright © 2014-2015, Stephen Commisso, Mikhael Filippov")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.7.7")]
-[assembly: AssemblyFileVersion("0.7.7")]
+[assembly: AssemblyVersion("0.8.0")]
+[assembly: AssemblyFileVersion("0.8.0")]
