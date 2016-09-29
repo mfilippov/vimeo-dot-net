3cf9e3005f8196fafff120dec1e43921f67cf25f
Mikhail Filippov <Mikhail.Filippov@jetbrains.com>
2016-09-29 21:37:18 +0300

    Remove fake target frameworks project.

--- src/VimeoDotNet.Net451/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("VimeoDotNet")]
-[assembly: AssemblyDescription("Core library for accessing the Vimeo REST API")]
-
-[assembly: Guid("1007d518-224f-461b-8331-9ebdd15aa220")]
