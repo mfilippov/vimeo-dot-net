d4eed1b4e450b528e2a4102b224ae513dcba3cb1
Mikhail Filippov <Mikhail.Filippov@jetbrains.com>
2016-09-29 21:43:28 +0300

    Prepare AssemblyInfo for patching from appVeyor.

--- src/VimeoDotNet.Tests/Properties/AssemblyInfo.cs
+++ src/VimeoDotNet.Tests/Properties/AssemblyInfo.cs
@@ -2,6 +2,12 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("VimeoDotNet.Tests")]
+[assembly: AssemblyCompany("VimeoDotNet")]
+[assembly: AssemblyProduct("VimeoDotNet")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016, Stephen Commisso, Mikhael Filippov")]
 [assembly: AssemblyDescription("Test library for VimeoDotNet")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
 
 [assembly: Guid("8885a955-0576-487a-a7f2-bb20cd71ad6b")]
