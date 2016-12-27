8c0900d7f72669496c75eeeef5feaa84fcac0b1a
Mikhail Filippov <Mikhail.Filippov@jetbrains.com>
2016-12-27 21:49:00 +0300

    Make VimeoApiException public, fixed #66

--- src/VimeoDotNet/Exceptions/VimeoApiException.cs
+++ src/VimeoDotNet/Exceptions/VimeoApiException.cs
@@ -3,23 +3,41 @@ using System.Runtime.Serialization;
 
 namespace VimeoDotNet.Exceptions
 {
+    /// <summary>
+    /// VimeoApiException
+    /// </summary>
     [Serializable]
-    internal class VimeoApiException : ApplicationException
+    public class VimeoApiException : ApplicationException
     {
+        /// <summary>
+        /// Create new VimeoApiException
+        /// </summary>
         public VimeoApiException()
         {
         }
 
+        /// <summary>
+        /// Create new VimeoApiException with message
+        /// </summary>
+        /// <param name="message">Message</param>
         public VimeoApiException(string message)
             : base(message)
         {
         }
 
+        /// <summary>
+        /// Create new VimeoApiException with message and inner exception
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <param name="innerException"></param>
         public VimeoApiException(string message, Exception innerException)
             : base(message, innerException)
         {
         }
 
+        /// <summary>
+        /// Create new VimeoApiException
+        /// </summary>
         public VimeoApiException(SerializationInfo info, StreamingContext context)
             : base(info, context)
         {
