eb7309a70a7d53d4773d5ffdaaf8b06485d2bd0d
Mikhail Filippov <mikhail@filippov.me>
2019-01-24 11:06:29 +0300

    Disable test parallelism.

--- src/VimeoDotNet.Tests/Properties/AssemblyInfo.cs
+++ src/VimeoDotNet.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Xunit;
 
 [assembly: AssemblyTitle("VimeoDotNet.Tests")]
 [assembly: AssemblyCompany("VimeoDotNet")]
@@ -9,5 +10,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyFileVersion("2.0.0")]
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
+
 
 [assembly: Guid("8885a955-0576-487a-a7f2-bb20cd71ad6b")]
