af2405bec1ef1bafcf59f286169d2362a430e19f
Mikhail Filippov <mikhail@filippov.me>
2023-04-09 23:19:20 +0400

    Code cleanup

--- src/VimeoDotNet/VimeoClient_Folders.cs
+++ src/VimeoDotNet/VimeoClient_Folders.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using VimeoDotNet.Constants;
 using VimeoDotNet.Exceptions;
