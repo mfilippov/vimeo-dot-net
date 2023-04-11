937452db3251035586988c58fe3c8d495dde3bc0
cboyce428 <cboyce428@gmail.com>
2023-04-11 05:15:18 +0100

    Summary Text
    As per https://developer.vimeo.com/api/reference/videos#upload_video

--- src/VimeoDotNet/Enums/VideoPrivacyEnum.cs
+++ src/VimeoDotNet/Enums/VideoPrivacyEnum.cs
@@ -1,4 +1,6 @@
-﻿namespace VimeoDotNet.Enums
+﻿using System;
+
+namespace VimeoDotNet.Enums
 {
     /// <summary>
     /// View privacy
@@ -6,37 +8,40 @@
     public enum VideoPrivacyEnum
     {
         /// <summary>
-        /// Nobody
+        /// Nobody - No one except the owner can access the video. This privacy setting appears as Private on the Vimeo front end.
         /// </summary>
         Nobody,
 
         /// <summary>
-        /// Anybody
+        /// Anybody - Anyone can access the video. This privacy setting appears as Public on the Vimeo front end.
         /// </summary>
         Anybody,
 
         /// <summary>
-        /// Only contacts
+        /// Contacts - Only those who follow the owner on Vimeo can access the video.
         /// </summary>
+        [Obsolete]
         Contacts,
 
         /// <summary>
-        /// Only users
+        /// Users - Only Vimeo members can access the video.
         /// </summary>
+        [Obsolete]
         Users,
 
         /// <summary>
-        /// Password
+        /// Password - Only those with the password can access the video.
         /// </summary>
         Password,
 
         /// <summary>
-        /// Disable
+        /// Disable - The video is embeddable, but it's hidden on Vimeo and can't be played. This privacy setting appears as Hide from Vimeo on the Vimeo front end.
         /// </summary>
+        [Obsolete]
         Disable,
 
         /// <summary>
-        /// Unlisted
+        /// Unlisted - Only those with the private link can access the video.
         /// </summary>
         Unlisted
     }
